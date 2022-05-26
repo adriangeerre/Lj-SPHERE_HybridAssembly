a4ca693fb9e3f4abc67f2a47e7a97c9e6825a92a
adriangeerre <47116776+adriangeerre@users.noreply.github.com>
2022-05-26 16:06:37 +0200

    scratch
    Add diamonds scratch temporal directory

--- pipeline_HA_flye.py
+++ pipeline_HA_flye.py
@@ -340,6 +340,7 @@ def diamond(out_dir, diamond_db, threads, memory, folder):
 	# Folder structure
 	if os.path.isdir("80-Validation") == False: os.mkdir("80-Validation")
 	if os.path.isdir("80-Validation/" + folder) == False: os.mkdir("80-Validation/" + folder)
+	if os.path.isdir("80-Validation/" + folder + "/Diamond") == False: os.mkdir("80-Validation/" + folder + "/Diamond")
 
 	# GWF
 	inputs = ["70-Prokka/{}/prokka_{}.faa".format(folder, folder)]
@@ -351,7 +352,7 @@ def diamond(out_dir, diamond_db, threads, memory, folder):
 
 	# Diamond (db pre-computed)
 	conda activate Diamond
-	diamond blastp --query 70-Prokka/{folder}/prokka_{folder}.faa --db {diamond_db} --outfmt 6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore slen qlen sseq qseq --threads {threads} --out {out_dir}/{folder}.diamond.tsv --un {out_dir}/{folder}.diamond.unalign.fasta --unfmt fasta --fast
+	diamond blastp --query 70-Prokka/{folder}/prokka_{folder}.faa --db {diamond_db} --outfmt 6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore slen qlen sseq qseq --threads {threads} --out {out_dir}/{folder}.diamond.tsv --un {out_dir}/{folder}.diamond.unalign.fasta --unfmt fasta --fast --tmpdir /scratch/$SLURM_JOBID/ --parallel-tmpdir /scratch/$SLURM_JOBID/
 	'''.format(out_dir=out_dir, diamond_db=diamond_db, folder=folder, threads=threads)
 
 	return AnonymousTarget(inputs=inputs, outputs=outputs, options=options, spec=spec)
