24f4141d1687d749dd8635c6ac1b270fe568c97a
adriangeerre <adriangomezrepolles@gmail.com>
2022-09-20 13:33:57 +0200

    cpu_mem_annot

--- pipeline_HA_flye.py
+++ pipeline_HA_flye.py
@@ -477,7 +477,7 @@ for row in f:
 		pgap_files_creator(genus = genus, assembly = "30-Unicycler/{}/flye/assembly.fasta".format(folder), out_dir = out_dir_yaml)
 		
 	if os.path.exists(out_dir_yaml + '.submol.yml') and os.path.exists(out_dir_yaml + '.input.yml') and genus != "NA":
-		gwf.target_from_template("{}_70_annotation".format(folder), annotation(assembly="30-Unicycler/{}/flye/assembly.fasta".format(folder), input_yaml = "{}.input.yml".format(out_dir_yaml), out_dir="70-Annotation/{}/annotation".format(folder), threads=4, memory=16, folder=folder))
+		gwf.target_from_template("{}_70_annotation".format(folder), annotation(assembly="30-Unicycler/{}/flye/assembly.fasta".format(folder), input_yaml = "{}.input.yml".format(out_dir_yaml), out_dir="70-Annotation/{}/annotation".format(folder), threads=1, memory=4, folder=folder))
 
 	# 80 Validation
 	database = busco_dict[LjTaxa[folder]]
