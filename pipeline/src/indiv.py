716e8274460de57a6347ece850de44d5f0770bc3
adriangeerre <adriangomezrepolles@gmail.com>
2023-01-09 13:52:43 +0100

    indiv_no_contigs_flow

--- pipeline/src/indiv.py
+++ pipeline/src/indiv.py
@@ -44,6 +44,19 @@ def exec_info(read1, read2, long, prefix, order, genus, threads, memory, run_cov
     return(message)
 
 # Functions
+def coverage(assembly_folder, prefix, software, reads_1, reads_2, threads, cpath):
+    # Align illumina
+    coverage.align_illumina(assembly=f"{assembly_folder}/{prefix}/{software}/assembly.fasta", illumina_corr_1=reads_1, illumina_corr_2=reads_2, out_dir=f"{assembly_folder}/{prefix}/Align", threads=threads, conda_path=cpath, logfile=f".logs/{prefix}.log")
+
+    # Align nanopore
+    coverage.align_nanopore(assembly=f"{assembly_folder}/{prefix}/{software}/assembly.fasta", nanopore_corr=f"10-Correction/{prefix}/Nanopore/nanopore.corrected.fasta", out_dir=f"{assembly_folder}/{prefix}/Align", threads=threads, conda_path=cpath, logfile=f".logs/{prefix}.log")
+    
+    # Obtain coverage
+    coverage.coverage(in_dir="{assembly_folder}/{prefix}/Align", out_dir=f"{assembly_folder}/{prefix}/Coverage", conda_path=cpath, logfile=f".logs/{prefix}.log")
+
+    # Plot coverage
+    coverage.plot_coverage(in_dir=f"{assembly_folder}/{prefix}/Align", out_dir=f"{assembly_folder}/{prefix}/Coverage/CovPlots", prefix=prefix, conda_path=cpath, logfile=f".logs/{prefix}.log")
+
 def conda_path():
     # Exec terminal command
     cmd = "conda info | grep 'base environment' | awk '{print $4}'"
@@ -54,23 +67,6 @@ def conda_path():
     out = str(out.strip()).replace("'","")[1:]
 
     return [out, err]
-
-def check_contig_number(hybrid_assembly, nanopore_draft):
-    # Count contigs
-    cmdd = f"grep -c '^>' {nanopore_draft}"
-    cmdh = f"grep -c '^>' {hybrid_assembly}"
-    dp = subprocess.Popen(cmdd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    hp = subprocess.Popen(cmdh, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    outd,errd = dp.communicate()
-    outh,errh = hp.communicate()
-
-    # Return category
-    if int(outd) == int(outh):
-        return "Equal"
-    elif int(outd) < int(outh):
-        return "Below"
-    else:
-        return "Above" # Exit
 
 def init(read1, read2, long, prefix, order, genus, threads, memory, run_coverage):
 
@@ -95,7 +91,6 @@ def init(read1, read2, long, prefix, order, genus, threads, memory, run_coverage
 
     # QC
     #---
-
     logger.info('**** QC ****')
 
     # Illumina QC
@@ -110,7 +105,6 @@ def init(read1, read2, long, prefix, order, genus, threads, memory, run_coverage
     
     # Correction
     #-----------
-
     logger.info('**** Illumina Correction ****')
 
     # Define Illumina correct output
@@ -129,7 +123,6 @@ def init(read1, read2, long, prefix, order, genus, threads, memory, run_coverage
 
     # Assembly
     #---------
-
     logger.info('**** Nanopore Draft Assembly ****')
 
     # Nanopore draft (Flye)
@@ -142,60 +135,19 @@ def init(read1, read2, long, prefix, order, genus, threads, memory, run_coverage
     if not os.path.exists(f"30-HybridAssembly/{prefix}/unicycler/assembly.fasta"):
         assembly.unicycler(assembly=f"20-Assembly/{prefix}/flye/assembly.fasta", illumina_corr_1=f"10-Correction/{prefix}/Illumina/corrected/{read_corr_1}", illumina_corr_2=f"10-Correction/{prefix}/Illumina/corrected/{read_corr_2}", nanopore_corr=f"10-Correction/{prefix}/Nanopore/nanopore.corrected.fasta", out_dir=f"30-HybridAssembly/{prefix}/unicycler", threads=threads, conda_path=cpath, logfile=f".logs/{prefix}.log")
 
-    # Check contigs
-    #--------------
-    d = f"20-Assembly/{prefix}/flye/assembly.fasta"
-    h = f"30-HybridAssembly/{prefix}/unicycler/assembly.fasta"
-    
-    if os.path.exists(h) and os.path.exists(d):
-        category = check_contig_number(h, d)
-    elif not os.path.exists(h) and os.path.exists(d):
-        category = "Below"
-
-    logger.info(f'Category: {category}')
-
-    # Define path
-    #------------
-
-    if category == "Equal":
-        assembly_folder = "30-HybridAssembly"
-        software = "unicycler"
-    elif category == "Below":
-        assembly_folder = "20-Assembly"
-        software = "flye"
-
-    logger.info(f'Assembly folder: {assembly_folder}')
-    logger.info(f'Assembly software: {software}')
-
-    # Coverage
-    #---------
-    
-    if run_coverage == True: # Define title for Draft or HA
-
-        logger.info('**** Running coverage ****')
-
-        # Align illumina
-        coverage.align_illumina(assembly=f"{assembly_folder}/{prefix}/{software}/assembly.fasta", illumina_corr_1=read_corr_1, illumina_corr_2=read_corr_2, out_dir=f"{assembly_folder}/{prefix}/Align", threads=threads, conda_path=cpath, logfile=f".logs/{prefix}.log")
-
-        # Align nanopore
-        coverage.align_nanopore(assembly=f"{assembly_folder}/{prefix}/{software}/assembly.fasta", nanopore_corr=f"10-Correction/{prefix}/Nanopore/nanopore.corrected.fasta", out_dir=f"{assembly_folder}/{prefix}/Align", threads=threads, conda_path=cpath, logfile=f".logs/{prefix}.log")
-        
-        # Obtain coverage
-        coverage.coverage(in_dir="{assembly_folder}/{prefix}/Align", out_dir=f"{assembly_folder}/{prefix}/Coverage", conda_path=cpath, logfile=f".logs/{prefix}.log")
-
-        # Plot coverage
-        coverage.plot_coverage(in_dir=f"{assembly_folder}/{prefix}/Align", out_dir=f"{assembly_folder}/{prefix}/Coverage/CovPlots", prefix=prefix, conda_path=cpath, logfile=f".logs/{prefix}.log")
-
     # Validation
     #-----------
+    logger.info('**** Hybrid Assembly summary: Quast ****')
 
-    logger.info('**** Genome Validation ****')
+    # Folders
+    assembly_folder="30-HybridAssembly"
+    software="unicycler"
 
     # Quast
     if not os.path.exists(f"{assembly_folder}/{prefix}/Quast/report.txt"):
         validation.quast(assembly=f"{assembly_folder}/{prefix}/{software}/assembly.fasta", out_dir=f"{assembly_folder}/{prefix}/Quast", threads=threads, conda_path=cpath, logfile=f".logs/{prefix}.log")
 
-    logger.info('**** Assembly validation: Busco and CheckM ****')
+    logger.info('**** Hybrid Assembly validation: Busco and CheckM ****')
 
     # Assembly validation
     busco_dict = {'Actinomycetales': 'actinobacteria_class_odb10', 'Flavobacteriales': 'flavobacteriales_odb10', 'Bacillales': 'bacillales_odb10', 'Burkholderiales': 'burkholderiales_odb10', 'Caulobacterales': 'alphaproteobacteria_odb10', 'Rhizobiales': 'rhizobiales_odb10', 'Sphingomonadales': 'sphingomonadales_odb10', 'Pseudomonadales': 'pseudomonadales_odb10', 'Xanthomonadales': 'xanthomonadales_odb10'}
@@ -216,12 +168,12 @@ def init(read1, read2, long, prefix, order, genus, threads, memory, run_coverage
     # Validate BUSCO
     if os.path.isdir(f"40-Validation/{prefix}/{software}/Busco"):
         try:
-            b = [i for i in os.listdir(f"40-Validation/{prefix}/{software}/Busco") if i[-5:] == ".json"][0]
-            bf = open(f"40-Validation/{prefix}/{software}Busco/{b}")
-            bd = json.load(bf)
-            bv = validation.validate_busco(bd)
-            bf.close()
-            logger.info(f'Busco validation: {bv}')
+            bha = [i for i in os.listdir(f"40-Validation/{prefix}/{software}/Busco") if i[-5:] == ".json"][0]
+            bfha = open(f"40-Validation/{prefix}/{software}Busco/{bha}")
+            bdha = json.load(bfha)
+            bvha = validation.validate_busco(bdha)
+            bfha.close()
+            logger.info(f'Busco validation: {bvha}')
         except:
             logger.error(f"Folder \"40-Validation/{prefix}/{software}/Busco\" is missing or empty.")
             pass
@@ -230,16 +182,19 @@ def init(read1, read2, long, prefix, order, genus, threads, memory, run_coverage
     if os.path.isdir(f"40-Validation/{prefix}/{software}/CheckM"):
         c = f"40-Validation/{prefix}/{software}/CheckM/results.tsv"
         try:
-            cv = validation.validate_checkm(c)
-            logger.info(f'CheckM validation: {cv}')
+            cvha = validation.validate_checkm(c)
+            logger.info(f'CheckM validation: {cvha}')
         except:
             logger.error(f"File \"{c}\" is missing or empty.")
             pass
 
     # Annotation
     #-----------
-    if "bv" in globals() and "cv" in globals():
-        if bv == "Pass" and cv == "Pass":
+    if "bvha" in globals() and "cvha" in globals():
+        if bvha == "Pass" and cvha == "Pass":
+            logger.info('**** Hybrid Assembly annotation: PGAP ****')
+
+            # Annotation of Hybrid Assembly
             if os.path.isdir(f"{assembly_folder}/{prefix}/{software}"):
                 out_dir_yaml = f"{assembly_folder}/{prefix}/{software}/{prefix}"
                 if not os.path.exists(out_dir_yaml + ".submol.yml") and not os.path.exists(out_dir_yaml + ".input.yml"):
@@ -248,53 +203,137 @@ def init(read1, read2, long, prefix, order, genus, threads, memory, run_coverage
                 if os.path.exists(out_dir_yaml + '.submol.yml') and os.path.exists(out_dir_yaml + '.input.yml') and genus != "NA":
                     annotation.annotation(input_yaml=f"{out_dir_yaml}.input.yml", out_dir=f"50-Annotation/{prefix}/{software}", memory=4, threads=1,  conda_path=cpath, logfile=f".logs/{prefix}.log")
 
+            # Validate Hybrid Assembly annotation
             if os.path.isdir(f"50-Annotation/{prefix}/{software}/annotation"):
                 a = f"50-Annotation/{prefix}/{software}/annotation/annot.gff"
                 try:
-                    av = validation.validate_pgap(a)                
+                    avha = validation.validate_pgap(a)                
                 except:
                     if os.path.isdir(f"50-Annotation/{prefix}/annotation"):
                         logger.error(f"File \"{a}\" is missing or empty.")
                         pass
-        elif bv == "Fail" or cv == "Fail":
-            pass
 
-            # 16S genus identification
-            #new_genus = taxonomy.taxonomy(assembly=f"{assembly_folder}/{prefix}/{software}/assembly.fasta")
-
-            if genus != new_genus and os.path.isdir(f"{assembly_folder}/{prefix}/{software}"):
-                out_dir_yaml = f"{assembly_folder}/{prefix}/{software}/{prefix}"
-                if not os.path.exists(out_dir_yaml + ".submol.yml") and not os.path.exists(out_dir_yaml + ".input.yml"):
-                    annotation.pgap_files_creator(genus = new_genus, assembly = f"{assembly_folder}/{prefix}/{software}/assembly.fasta", out_dir = out_dir_yaml)
+            # Complete Genome
+            if "avha" in globals():
+                if avha == "Pass":
+                    # Create folder
+                    if not os.path.isdir("60-Genomes/Complete"): os.makedirs("60-Genomes/Complete")
                     
-                if os.path.exists(out_dir_yaml + '.submol.yml') and os.path.exists(out_dir_yaml + '.input.yml') and genus != "NA":
-                    annotation.annotation(input_yaml=f"{out_dir_yaml}.input.yml", out_dir=f"50-Annotation/{prefix}/{software}", memory=4, threads=1,  conda_path=cpath, logfile=f".logs/{prefix}.log")
+                    # Move assembly to Complete Genome
+                    os.system(f"cp {assembly_folder}/{prefix}/unicycler/assembly.fasta 60-Genomes/Complete/{prefix}.assembly")
+                    
+                    # Remove assembly from Improved Genome
+                    if os.path.exists(f"60-Genomes/Improved/{prefix}.assembly"): os.remove(f"60-Genomes/Improved/{prefix}.assembly.fasta")
 
-                if os.path.isdir(f"50-Annotation/{prefix}/{software}/annotation"):
-                    a = f"50-Annotation/{prefix}/{software}/annotation/annot.gff"
-                    try:
-                        av = validation.validate_pgap(a)
-                    except:
-                        if os.path.isdir(f"50-Annotation/{prefix}/annotation"):
-                            # logger.error(f"File \"{a}\" is missing or empty (re-annotation).")
-                            pass
-        else:
-            # Assembly did not pass
-            pass
+                    # Coverage
+                    if run_coverage == True:
+                        # Logger
+                        logger.info('**** Running coverage HA ****')
 
-        # Organize genomes
-        if av == "Pass" and category == "Equal":
-            # Create folder
-            if not os.path.isdir("60-Genomes/Complete"): os.makedirs("60-Genomes/Complete")
-            
-            # Move assembly to Complete Genome
-            os.system(f"cp {assembly_folder}/{prefix}/unicycler/assembly.fasta 60-Genomes/Complete/{prefix}.assembly")
-            
-            # Remove assembly from Improved Genome
-            if os.path.exists(f"60-Genomes/Improved/{prefix}.assembly"): os.remove(f"60-Genomes/Improved/{prefix}.assembly.fasta")            
-        elif av == "Pass" and category == "Below":
-            # Create folder
-            if not os.path.isdir("60-Genomes/Improved"): os.makedirs("60-Genomes/Improved")
-            
-            # Move assembly to Improved Genome
-            os.system(f"cp {assembly_folder}/{prefix}/flye/assembly.fasta 60-Genomes/Improved/{prefix}.assembly.fasta")
+                        # Call
+                        coverage(assembly_folder=assembly_folder, prefix=prefix, software=software, reads_1=read_corr_1, reads2=read_corr_2, threads=threads, cpath=cpath)
+
+                else:
+                    logger.warning(f"Sample {prefix} failed the hybrid assembly annotation validation.")
+            else:
+                logger.error(f"Hybrid assembly annotation validation for sample {prefix} was not completed. Please, check the logs.")
+        
+        # HA assembly or annotation failed (all variables already checked in globals)
+        if bvha == "Fail" or cvha == "Fail" or avha == "Fail":
+            # Validation failed (Draft)
+            logger.warning(f"Sample {prefix} failed the assembly or annotation validation at the hybrid assembly level. Instead, the nanopore draft will be validated in search to recover an assembly for {prefix}.")
+
+            # Use Draft
+            logger.info('**** Draft Assembly validation: Busco and CheckM ****')
+
+            # Folders
+            assembly_folder="20-Assembly"
+            software="flye"
+
+            try:
+                if not os.path.exists(f"40-Validation/{prefix}/{software}/Busco/short_summary.specific.{database}.Busco.txt") or not os.path.exists(f"40-Validation/{prefix}/{software}/CheckM/results.tsv") :
+                    validation.assembly_validation(assembly=f"{assembly_folder}/{prefix}/{software}/assembly.fasta", database=database, out_dir=f"40-Validation/{prefix}/{software}", threads=threads, conda_path=cpath, logfile=f".logs/{prefix}.log")
+            except:
+                logger.error(f"Database for {order} not considered for Busco execution. You can modify the object \"busco_dict\" to include the required database. Please, check \"busco --list-datasets\" to know which Order should be selected.")
+                pass
+
+            # Validate Draft BUSCO
+            if os.path.isdir(f"40-Validation/{prefix}/{software}/Busco"):
+                try:
+                    bd = [i for i in os.listdir(f"40-Validation/{prefix}/{software}/Busco") if i[-5:] == ".json"][0]
+                    bfd = open(f"40-Validation/{prefix}/{software}Busco/{bd}")
+                    bdd = json.load(bfd)
+                    bvd = validation.validate_busco(bdd)
+                    bfd.close()
+                    logger.info(f'Busco Draft validation: {bvd}')
+                except:
+                    logger.error(f"Folder \"40-Validation/{prefix}/{software}/Busco\" is missing or empty.")
+                    pass
+
+            # Validate Draft CheckM
+            if os.path.isdir(f"40-Validation/{prefix}/{software}/CheckM"):
+                c = f"40-Validation/{prefix}/{software}/CheckM/results.tsv"
+                try:
+                    cvd = validation.validate_checkm(c)
+                    logger.info(f'CheckM Draft validation: {cvd}')
+                except:
+                    logger.error(f"File \"{c}\" is missing or empty.")
+                    pass
+
+            # Annotation
+            if "bvd" in globals() and "cvd" in globals():
+                if bvd == "Pass" and cvd == "Pass":
+                    logger.info('**** Draft Assembly annotation: PGAP ****')
+
+                    # Annotation of Hybrid Assembly
+                    if os.path.isdir(f"{assembly_folder}/{prefix}/{software}"):
+                        out_dir_yaml = f"{assembly_folder}/{prefix}/{software}/{prefix}"
+                        if not os.path.exists(out_dir_yaml + ".submol.yml") and not os.path.exists(out_dir_yaml + ".input.yml"):
+                            annotation.pgap_files_creator(genus = genus, assembly = f"{assembly_folder}/{prefix}/{software}/assembly.fasta", out_dir = out_dir_yaml)
+                            
+                        if os.path.exists(out_dir_yaml + '.submol.yml') and os.path.exists(out_dir_yaml + '.input.yml') and genus != "NA":
+                            annotation.annotation(input_yaml=f"{out_dir_yaml}.input.yml", out_dir=f"50-Annotation/{prefix}/{software}", memory=4, threads=1,  conda_path=cpath, logfile=f".logs/{prefix}.log")
+
+                    # Validate Hybrid Assembly annotation
+                    if os.path.isdir(f"50-Annotation/{prefix}/{software}/annotation"):
+                        a = f"50-Annotation/{prefix}/{software}/annotation/annot.gff"
+                        try:
+                            avd = validation.validate_pgap(a)                
+                        except:
+                            if os.path.isdir(f"50-Annotation/{prefix}/annotation"):
+                                logger.error(f"File \"{a}\" is missing or empty.")
+                                pass
+                    
+                    # Improved Genome
+                    if "avd" in globals():
+                        if avd == "Pass":
+                            # Create folder
+                            if not os.path.isdir("60-Genomes/Improved"): os.makedirs("60-Genomes/Improved")
+                            
+                            # Move assembly to Improved Genome
+                            os.system(f"cp {assembly_folder}/{prefix}/flye/assembly.fasta 60-Genomes/Improved/{prefix}.assembly.fasta")
+
+                            # Coverage
+                            if run_coverage == True:
+                                # Logger
+                                logger.info('**** Running coverage Draft ****')
+
+                                # Call
+                                coverage(assembly_folder=assembly_folder, prefix=prefix, software=software, reads_1=read_corr_1, reads2=read_corr_2, threads=threads, cpath=cpath)
+
+                        else:
+                            logger.warning(f"Sample {prefix} failed the draft annotation validation. Sample will be drop and no genome will be reported.")
+                    else:
+                        logger.error(f"Draft annotation validation for sample {prefix} was not completed. Please, check the logs.")
+                else:
+                    # Validation failed (Draft)
+                    logger.warning(f"Sample {prefix} failed the assembly validation at the nanopore draft level (after failing the hybrid assembly validation). Sample will be drop and no genome will be reported.")
+            else:
+                # No validation variables (Draft)
+                logger.error(f"Assembly validation for sample {prefix} was not completed. Please, check the logs.")
+    else:
+        # No validation variables (HA)
+        logger.error(f"Validation for sample {prefix} was not completed. Please, check the logs.")
+
+
+
